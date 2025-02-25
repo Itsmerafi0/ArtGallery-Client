a2736a783e7a1c188e0d997b3658ae8aa5bac486
Rafi Azhar <itsmerafi0@gmail.com>
2025-02-25 15:33:57 +0700

    feat: add new api hotarticles

--- src/api/eventApi.js
+++ src/api/eventApi.js
@@ -21,3 +21,25 @@ export async function getDetailEvent(itemId) {
     return null;
   }
 }
+
+export async function getEventOccurrences(page) {
+  try {
+    const response = await fetch(`${BASE_URL}event-occurrences?page=${page}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching event occurrences:", error);
+    return null;
+  }
+}
+
+export async function getDetailEventOccurrences(itemId) {
+  try {
+    const response = await fetch(`${BASE_URL}event-occurrences/${itemId}`);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching detail event:", error);
+    return null;
+  }
+}
